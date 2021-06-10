8038e9123b015cd5fb1d5e1fca22f52452606f36
NKaiiii <chatchaya1998@hotmail.com>
2021-06-10 17:54:29 +0700

    update .pkl file

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Api
+from pythainlp import word_tokenize
+import joblib
+import dill as pickle
+
+app = Flask(__name__)
+api = Api(app)
+
+# Load Model
+clf_path = 'models/fic-model.pkl'
+with open(clf_path,'rb') as f:
+    model = joblib.load(f)
+
+# Load Tfidf
+tfidf_path = 'models/fic-tfidf.pkl'
+with open(tfidf_path,'rb') as f:
+    tfidf = pickle.load(f)
+
+@app.route("/")
+def home():
+    return make_response("Fiction Gene Model Service", 200)
+
+@app.route("/predict", methods=['POST','GET'])
+def prediction():
+    if request.method == 'GET':
+        return make_response('Resend with POST Method', 200)
+    
+    elif request.method == 'POST':
+        x = request.get_json()
+        x_content = x['content']
+        x_token = word_tokenize(x_content)
+        x_trans = tfidf.transform([x_token])
+        
+        prob_raw = model.predict_proba(x_trans).round(4)
+        prob = prob_raw[0]
+        prob_genre = []
+        for i in range(0, len(model.classes_)):
+            sub = {
+                'genre': model.classes_[i],
+                'prob': prob[i]
+            }
+            prob_genre.append(sub)
+        prob_genre.sort(key=lambda x:x['prob'],reverse=True)
+
+        return make_response(jsonify(prob_genre), 200)
+
+if __name__ == "__main__":
+    app.run(debug=True)
